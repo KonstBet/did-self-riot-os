53e27e539142434b09858d475e226f5525042ba6
Konstantinos Betchavas <kostasbetchavas@hotmail.com>
2022-11-29 16:52:58 +0200

    hex to base64 & hash function & comments

--- coap_riot/coap_handler.c
+++ coap_riot/coap_handler.c
@@ -21,41 +21,14 @@
 #include "base64.h"
 
 /* digital signature key pair */ /* Generated using ed25519-genkeypair */
+static uint8_t* secret_key_bytes;
+static uint8_t* public_key_bytes;
 static char* secret_key_base64;
-static uint8_t secret_key_hex[100] = { 0 };
-// static uint8_t secret_key[EDSIGN_SECRET_KEY_SIZE] = { 0 };
 static char* public_key_base64;
-static uint8_t public_key_hex[100] = { 0 };
-// static uint8_t public_key[EDSIGN_PUBLIC_KEY_SIZE] = { 0 };
 
 /* hardcoded digital signature key pair */
-static uint8_t secret_key_hex_hardcoded[100] = "8bb4014d8b0a63af72d88482c1276ccd032e26fc05806886a9f1a727210f4fc3";
-//static uint8_t secret_key_hex_hardcoded[EDSIGN_SECRET_KEY_SIZE] = { 0 };
-
-static uint8_t public_key_hex_hardcoded[100] = "d04e907192471c603e148d73d6a3897976dc260106aa120837ebcf815f0445c2";
-//static uint8_t public_key_hex_hardcoded[EDSIGN_PUBLIC_KEY_SIZE] = { 0 };
-
-/* internal value that can be read/written via CoAP */
-static uint8_t internal_value = 0;
-
-static const uint8_t block2_intro[] = "This is RIOT (Version: ";
-static const uint8_t block2_board[] = " running on a ";
-static const uint8_t block2_mcu[] = " board with a ";
-
-static ssize_t _echo_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, void *context)
-{
-    (void)context;
-    char uri[CONFIG_NANOCOAP_URI_MAX];
-
-    if (coap_get_uri_path(pkt, (uint8_t *)uri) <= 0) {
-        return coap_reply_simple(pkt, COAP_CODE_INTERNAL_SERVER_ERROR, buf,
-                                 len, COAP_FORMAT_TEXT, NULL, 0);
-    }
-    char *sub_uri = uri + strlen("/echo/");
-    size_t sub_uri_len = strlen(sub_uri);
-    return coap_reply_simple(pkt, COAP_CODE_CONTENT, buf, len, COAP_FORMAT_TEXT,
-                             (uint8_t *)sub_uri, sub_uri_len);
-}
+// static char secret_key_base64_hardcoded[100] = "i7QBTYsKY69y2ISCwSdszQMuJvwFgGiGqfGnJyEPT8M=";
+// static char public_key_base64_hardcoded[100] = "0E6QcZJHHGA+FI1z1qOJeXbcJgEGqhIIN+vPgV8ERcI=";
 
 static ssize_t _riot_board_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, void *context)
 {
@@ -63,136 +36,6 @@ static ssize_t _riot_board_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, vo
     return coap_reply_simple(pkt, COAP_CODE_205, buf, len,
             COAP_FORMAT_TEXT, (uint8_t*)RIOT_BOARD, strlen(RIOT_BOARD));
 }
-
-static ssize_t _riot_block2_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, void *context)
-{
-    (void)context;
-    coap_block_slicer_t slicer;
-    coap_block2_init(pkt, &slicer);
-    uint8_t *payload = buf + coap_get_total_hdr_len(pkt);
-
-    uint8_t *bufpos = payload;
-
-    bufpos += coap_put_option_ct(bufpos, 0, COAP_FORMAT_TEXT);
-    bufpos += coap_opt_put_block2(bufpos, COAP_OPT_CONTENT_FORMAT, &slicer, 1);
-    *bufpos++ = 0xff;
-
-    /* Add actual content */
-    bufpos += coap_blockwise_put_bytes(&slicer, bufpos, block2_intro, sizeof(block2_intro)-1);
-    bufpos += coap_blockwise_put_bytes(&slicer, bufpos, (uint8_t*)RIOT_VERSION, strlen(RIOT_VERSION));
-    bufpos += coap_blockwise_put_char(&slicer, bufpos, ')');
-    bufpos += coap_blockwise_put_bytes(&slicer, bufpos, block2_board, sizeof(block2_board)-1);
-    bufpos += coap_blockwise_put_bytes(&slicer, bufpos, (uint8_t*)RIOT_BOARD, strlen(RIOT_BOARD));
-    bufpos += coap_blockwise_put_bytes(&slicer, bufpos, block2_mcu, sizeof(block2_mcu)-1);
-    bufpos += coap_blockwise_put_bytes(&slicer, bufpos, (uint8_t*)RIOT_MCU, strlen(RIOT_MCU));
-    /* To demonstrate individual chars */
-    bufpos += coap_blockwise_put_char(&slicer, bufpos, ' ');
-    bufpos += coap_blockwise_put_char(&slicer, bufpos, 'M');
-    bufpos += coap_blockwise_put_char(&slicer, bufpos, 'C');
-    bufpos += coap_blockwise_put_char(&slicer, bufpos, 'U');
-    bufpos += coap_blockwise_put_char(&slicer, bufpos, '.');
-
-    unsigned payload_len = bufpos - payload;
-    return coap_block2_build_reply(pkt, COAP_CODE_205,
-                                   buf, len, payload_len, &slicer);
-}
-
-static ssize_t _riot_value_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, void *context)
-{
-    (void) context;
-
-    ssize_t p = 0;
-    char rsp[16];
-    unsigned code = COAP_CODE_EMPTY;
-
-    /* read coap method type in packet */
-    unsigned method_flag = coap_method2flag(coap_get_code_detail(pkt));
-
-    switch(method_flag) {
-    case COAP_GET:
-        /* write the response buffer with the internal value */
-        p += fmt_u32_dec(rsp, internal_value);
-        code = COAP_CODE_205;
-        break;
-    case COAP_PUT:
-    case COAP_POST:
-        if (pkt->payload_len < 16) {
-            /* convert the payload to an integer and update the internal value */
-            char payload[16] = { 0 };
-            memcpy(payload, (char*)pkt->payload, pkt->payload_len);
-            internal_value = strtol(payload, NULL, 10);
-            code = COAP_CODE_CHANGED;
-        }
-        else {
-            code = COAP_CODE_REQUEST_ENTITY_TOO_LARGE;
-        }
-    }
-
-    return coap_reply_simple(pkt, code, buf, len,
-            COAP_FORMAT_TEXT, (uint8_t*)rsp, p);
-}
-
-ssize_t _sha256_handler(coap_pkt_t* pkt, uint8_t *buf, size_t len, void *context)
-{
-    (void)context;
-
-    /* using a shared sha256 context *will* break if two requests are handled
-     * at the same time.  doing it anyways, as this is meant to showcase block1
-     * support, not proper synchronisation. */
-    static sha256_context_t sha256;
-
-    uint8_t digest[SHA256_DIGEST_LENGTH];
-
-    uint32_t result = COAP_CODE_204;
-
-    coap_block1_t block1;
-    int blockwise = coap_get_block1(pkt, &block1);
-
-    printf("_sha256_handler(): received data: offset=%u len=%u blockwise=%i more=%i\n", \
-            (unsigned)block1.offset, pkt->payload_len, blockwise, block1.more);
-
-    if (block1.offset == 0) {
-        puts("_sha256_handler(): init");
-        sha256_init(&sha256);
-    }
-
-    sha256_update(&sha256, pkt->payload, pkt->payload_len);
-
-    if (block1.more == 1) {
-        result = COAP_CODE_CONTINUE;
-    }
-
-    size_t result_len = 0;
-    if (!blockwise || !block1.more) {
-        puts("_sha256_handler(): finish");
-        sha256_final(&sha256, digest);
-        result_len = SHA256_DIGEST_LENGTH * 2;
-    }
-
-    ssize_t reply_len = coap_build_reply(pkt, result, buf, len, 0);
-    uint8_t *pkt_pos = (uint8_t*)pkt->hdr + reply_len;
-    if (blockwise) {
-        pkt_pos += coap_opt_put_block1_control(pkt_pos, 0, &block1);
-    }
-    if (result_len) {
-        *pkt_pos++ = 0xFF;
-        pkt_pos += fmt_bytes_hex((char *)pkt_pos, digest, sizeof(digest));
-    }
-
-    return pkt_pos - (uint8_t*)pkt->hdr;
-}
-
-
-
-
-//----------------------------------------------------------------------
-//----------------------------------------------------------------------
-//----------------------------------------------------------------------
-//<---------------------------  MY CODE  ------------------------------>
-//----------------------------------------------------------------------
-//----------------------------------------------------------------------
-
-
 
 //// getdiddocument() returns this document signed
     /*
@@ -206,21 +49,6 @@ ssize_t _sha256_handler(coap_pkt_t* pkt, uint8_t *buf, size_t len, void *context
 
         change sign function from hardcoded to created keys
     */
-
-// /** @brief   Convert hex string to base64 string
-// * @param[in]   in_hex     Hex string
-// * @param[out]  out_base64  Base64 string
-// * @returns      size of base64 string
-//  */
-// size_t hex_to_base64(uint8_t* in_hex, char* out_base64) {
-//     uint8_t toBytes[EDSIGN_PUBLIC_KEY_SIZE] = { 0 };
-//     fmt_hex_bytes(toBytes, (char*)in_hex); // convert hex to bytes
-
-//     size_t size;
-//     base64_encode(toBytes, sizeof(toBytes), out_base64, &size); // convert bytes to base64
-
-//     return size;
-// }
 
 /** @brief   Convert bytes to base64 string
 * @param[in]   in_bytes     Bytes
@@ -249,47 +77,65 @@ size_t base64_to_bytes(char* in_base64, uint8_t* out_bytes) {
     return size;
 }
 
+/** @brief   Hash string with SHA256
+ * @param[in]   str     String to hash
+ * @param[out]  hash    Hash of str
+ * returns 0 on success
+ */
+int hash_string(char *str, char *hash)
+{
+    uint8_t digest[SHA256_DIGEST_LENGTH];
+    sha256_context_t sha256;
+
+    sha256_init(&sha256);
+    sha256_update(&sha256, (uint8_t*)str, strlen(str));
+    sha256_final(&sha256, digest);
+
+    fmt_bytes_hex(hash, digest, sizeof(digest));
+
+    return 0;
+}
+
 /** @brief  Create public and private keys for DID
 * @param COAP-PARAMETERS
 * @returns "keys created" if success
 */
+/* -- COAP REQUEST --
+REQUEST: coap-client -m get coap://[fe80::7cde:caff:fe7f:ca57%tap0]/riot/createkeys
+RESPONSE: keys created
+*/
+/* -- PRINT AT DEVICE CONSOLE --
+New keypair generated(PRINT IN HEX TO VERIFY WITH BASE64):
+  - Secret key hex: 4881E2BA00000000000000000000000000000000000000000000000000000040
+  - Public key hex: 62FF3DADE2EFDA7C53D38F6005DF358946E22402E5CCDF2EBAC179E249F159FF
+  - Secret key base64: SIHiugAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEA=
+  - Public key base64: Yv89reLv2nxT049gBd81iUbiJALlzN8uusF54knxWf8=
+
+  //PRINT HEX AS WELL FOR DEBUGGING
+*/
 static ssize_t _create_keys_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, void *context)
 {
     (void)context;
-    uint8_t secret_key[EDSIGN_SECRET_KEY_SIZE] = { 0 };
-    uint8_t public_key[EDSIGN_PUBLIC_KEY_SIZE] = { 0 };
+    secret_key_bytes = malloc(EDSIGN_SECRET_KEY_SIZE);
+    public_key_bytes = malloc(EDSIGN_PUBLIC_KEY_SIZE);
 
     printf("RUNNING KEY HANDLER\n");
     /* Create the new keypair */ // Ed25519
-    random_bytes(secret_key, sizeof(secret_key));
-    ed25519_prepare(secret_key);
-    edsign_sec_to_pub(public_key, secret_key);
+    random_bytes(secret_key_bytes, sizeof(secret_key_bytes));
+    ed25519_prepare(secret_key_bytes);
+    edsign_sec_to_pub(public_key_bytes, secret_key_bytes);
 
     /* Print the new keypair */ //Prints the hex to compare with base64
-    puts("New keypair generated:");
-    printf("  - Secret: ");
-    for (uint8_t i = 0; i < EDSIGN_SECRET_KEY_SIZE; ++i) {
-        printf("%02X", secret_key[i]);
-    }
+    puts("New keypair generated(PRINT IN HEX TO VERIFY WITH BASE64):");
+    printf("  - Secret key hex: ");
+    for (uint8_t i = 0; i < EDSIGN_SECRET_KEY_SIZE; ++i)
+        printf("%02X", secret_key_bytes[i]);
 
-    printf("\n  - Public: ");
-    for (uint8_t i = 0; i < EDSIGN_PUBLIC_KEY_SIZE; ++i) {
-        printf("%02X", public_key[i]);
-    }
+    printf("\n  - Public key hex: ");
+    for (uint8_t i = 0; i < EDSIGN_PUBLIC_KEY_SIZE; ++i)
+        printf("%02X", public_key_bytes[i]);
     puts("");
 
-
-    //TRYING bytes_to_base64 and base64_to_bytes functions
-
-    // // convert public key to base64
-    // char* public_key_base64 = malloc(EDSIGN_PUBLIC_KEY_SIZE * 2);
-    // bytes_to_base64(public_key, EDSIGN_SECRET_KEY_SIZE, public_key_base64);
-    // printf("\n\nBase64: %s with size %d\n\n", public_key_base64, strlen(public_key_base64));
-
-    // //convert base64 to bytes
-    // uint8_t public_key_BYTES[EDSIGN_SECRET_KEY_SIZE] = { 0 };
-    // base64_to_bytes(public_key_base64, public_key_BYTES);
-    // printf("Compare: %d\n",memcmp(public_key_BYTES, public_key, sizeof(public_key)));
 
     free(secret_key_base64);
     free(public_key_base64);
@@ -297,10 +143,10 @@ static ssize_t _create_keys_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, v
     public_key_base64 = malloc(EDSIGN_PUBLIC_KEY_SIZE * 2);
 
     //SAVE KEYS TO BASE64
-    bytes_to_base64(secret_key, EDSIGN_SECRET_KEY_SIZE, secret_key_base64);
-    bytes_to_base64(public_key, EDSIGN_PUBLIC_KEY_SIZE, public_key_base64);
-    printf("\nSecret key base64: %s\n", secret_key_base64);
-    printf("\nPublic key base64: %s\n", public_key_base64);
+    bytes_to_base64(secret_key_bytes, EDSIGN_SECRET_KEY_SIZE, secret_key_base64);
+    bytes_to_base64(public_key_bytes, EDSIGN_PUBLIC_KEY_SIZE, public_key_base64);
+    printf("  - Secret key base64: %s\n", secret_key_base64);
+    printf("  - Public key base64: %s\n", public_key_base64);
 
     return coap_reply_simple(pkt, COAP_CODE_205, buf, len,
             COAP_FORMAT_TEXT, "keys created", 13);
@@ -309,6 +155,12 @@ static ssize_t _create_keys_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, v
 /** @brief  Get public key of DID
 * @param COAP-PARAMETERS
 * @returns public key of DID
+*/
+/* -- COAP REQUEST --
+REQUEST: coap-client -m get coap://[fe80::7cde:caff:fe7f:ca57%tap0]/riot/getpublickey
+RESPONSE: Yv89reLv2nxT049gBd81iUbiJALlzN8uusF54knxWf8= (SAME AS PRINTED IN DEVICE CONSOLE AT CREATEKEYS)
+*/
+/* -- PRINT AT DEVICE CONSOLE --
 */
 static ssize_t _get_public_key_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, void *context)
 {
@@ -331,33 +183,32 @@ static ssize_t _get_public_key_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len
 * @param[in] public_key public key
 * @returns message with signature as string ("message,signature")
 */
-char* signMessageAndReturnResponse(uint8_t* message, uint16_t message_len, uint8_t* secret_key, uint8_t* public_key) { // USED IN SIGN HANDLER
-
-    uint8_t signature[EDSIGN_SIGNATURE_SIZE];
-    // secret_key_bytes & public_key_bytes are used to make hex string to bytes
-    uint8_t secret_key_bytes[EDSIGN_SECRET_KEY_SIZE] = { 0 };
-    uint8_t public_key_bytes[EDSIGN_PUBLIC_KEY_SIZE] = { 0 };
-
-    //Turn keys from hex to bytes to be able to sign
-    fmt_hex_bytes(secret_key_bytes, (char*)secret_key);
-    fmt_hex_bytes(public_key_bytes, (char*)public_key);
+char* signMessageAndReturnResponse(uint8_t* message, uint16_t message_len, uint8_t* secret_key, uint8_t* public_key) // USED IN SIGN HANDLER
+{ 
+    uint8_t* signature = malloc(EDSIGN_SIGNATURE_SIZE);
 
     //Sign message
-    edsign_sign(signature, public_key_bytes, secret_key_bytes, message, message_len);
+    edsign_sign(signature, public_key, secret_key, message, message_len);
 
-    //CHECK WITH VERIFY IF SIGN WORKED
-    // int aaaa = edsign_verify(signature, public_key_bytes, message, message_len);
-    // printf("%d\n\n", aaaa);
+    //CHECK WITH VERIFY IF SIGN WORKED (MUST BE NOT 0)
+    int verify = edsign_verify(signature, public_key, message, message_len);
+    if (verify == 0)
+        printf("SIGNATURE NOT VERIFIED\n");
+    else
+        printf("SIGNATURE VERIFIED\n");
 
-    char signature_hex[EDSIGN_SIGNATURE_SIZE * 2 + 1] = { 0 };
-    fmt_bytes_hex(signature_hex, signature, EDSIGN_SIGNATURE_SIZE);
-    //printf("%s", signature_hex);
+    //Turn signature to base64 string
+    char* signature_base64 = malloc(EDSIGN_SIGNATURE_SIZE * 2);
+    size_t size = bytes_to_base64(signature, EDSIGN_SIGNATURE_SIZE, signature_base64);
 
     //Create response with signature
-    char *response = malloc(EDSIGN_SIGNATURE_SIZE * 2 + 1 + 1 + message_len); 
+    char *response = malloc(size + 1 + message_len); 
     memcpy(response, message, message_len);
     memcpy(response + message_len, ",", 1);
-    memcpy(response + message_len + 1, signature_hex, EDSIGN_SIGNATURE_SIZE * 2 + 1);
+    memcpy(response + message_len + 1, signature_base64, size);
+
+    free(signature);
+    free(signature_base64);
     
     return response;
 }
@@ -366,14 +217,25 @@ char* signMessageAndReturnResponse(uint8_t* message, uint16_t message_len, uint8
 * @param COAP-PARAMETERS
 * @returns message with signature as string ("message,signature")
 */
+/* -- COAP REQUEST --
+REQUEST: coap-client -m post coap://[fe80::7cde:caff:fe7f:ca57%tap0]/riot/sign -e message
+RESPONSE: message,/bbM2225+nZeRJ6aA6xmGJdM2Bbc3qFNXpBjzdK8l8PiQGgiqDLHMRuIZO9ZF6qksvo7yvZOBKd9nuCDSgeaBg==
+*/
+/* -- PRINT AT DEVICE CONSOLE --
+SIGNATURE VERIFIED
+
+Response: message,/bbM2225+nZeRJ6aA6xmGJdM2Bbc3qFNXpBjzdK8l8PiQGgiqDLHMRuIZO9ZF6qksvo7yvZOBKd9nuCDSgeaBg==
+
+//TESTING SIGN MESSAGE AND RETURN MESSAGE WITH SIGNATURE SEPARATED BY COMMA
+*/
 static ssize_t ed25519_sign_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, void *context)
 {
     (void)context;
 
     char *response;
     // sign message with hardcoded keys
-    response = signMessageAndReturnResponse(pkt->payload, pkt->payload_len, secret_key_hex_hardcoded, public_key_hex_hardcoded);
-    printf("Response: %s\n", response);
+    response = signMessageAndReturnResponse(pkt->payload, pkt->payload_len, secret_key_bytes, public_key_bytes);
+    printf("\nResponse: %s\n", response);
 
     // send back message and signature
     return coap_reply_simple(pkt, COAP_CODE_205, buf, len,
@@ -387,12 +249,12 @@ static ssize_t ed25519_sign_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, v
 char* createDidDocument(void) {
     char* did = malloc(100);
     strcat(did, "\"id\": \"did:self:");
-    strncat(did, (char*)public_key_hex, EDSIGN_PUBLIC_KEY_SIZE * 2);
+    strncat(did, public_key_base64, strlen(public_key_base64));
     strcat(did, "\"");
 
     char* didAttestation = malloc(100);
     strcat(didAttestation, "\"attestation\": \"");
-    strncat(didAttestation, (char*)public_key_hex, EDSIGN_PUBLIC_KEY_SIZE * 2);
+    strncat(didAttestation, public_key_base64, strlen(public_key_base64));
     strcat(didAttestation, "\"");
 
 
@@ -412,12 +274,22 @@ char* createDidDocument(void) {
 * @param COAP-PARAMETERS
 * @returns DID document
 */
+/* -- COAP REQUEST --
+REQUEST: coap-client -m get coap://[fe80::7cde:caff:fe7f:ca57%tap0]/riot/did
+RESPONSE: {"id": "did:self:Yv89reLv2nxT049gBd81iUbiJALlzN8uusF54knxWf8=", "attestation": "Yv89reLv2nxT049gBd81iUbiJALlzN8uusF54knxWf8="},Xq2loWtdtJ3dmw5ZercJutIE+fD9RYHpEWuiB9iVYHQCscGYDSorgeUCvMpiSwXD1Ao0z+/Cs5iqYoiNFTWRDw==
+*/
+/* -- PRINT AT DEVICE CONSOLE --
+SIGNATURE VERIFIED
+Response: {"id": "did:self:Yv89reLv2nxT049gBd81iUbiJALlzN8uusF54knxWf8=", "attestation": "Yv89reLv2nxT049gBd81iUbiJALlzN8uusF54knxWf8="},Xq2loWtdtJ3dmw5ZercJutIE+fD9RYHpEWuiB9iVYHQCscGYDSorgeUCvMpiSwXD1Ao0z+/Cs5iqYoiNFTWRDw==
+
+//DID STILL NOT IMPLEMENTED CORRECTLY
+*/
 static ssize_t getDidDocument(coap_pkt_t *pkt, uint8_t *buf, size_t len, void *context)
 {
     (void) context;
     char* didDocument = createDidDocument();
     
-    char* response = signMessageAndReturnResponse((uint8_t*)didDocument, strlen(didDocument), secret_key_hex, public_key_hex);
+    char* response = signMessageAndReturnResponse((uint8_t*)didDocument, strlen(didDocument), secret_key_bytes, public_key_bytes);
     printf("Response: %s\n", response);
     
     return coap_reply_simple(pkt, COAP_CODE_205, buf, len+2048, //INCREASE BUFFER SIZE TO SEND BIGGER RESPONSE
@@ -427,16 +299,11 @@ static ssize_t getDidDocument(coap_pkt_t *pkt, uint8_t *buf, size_t len, void *c
 /* must be sorted by path (ASCII order) */
 const coap_resource_t coap_resources[] = {
     COAP_WELL_KNOWN_CORE_DEFAULT_HANDLER,
-    { "/echo/", COAP_GET | COAP_MATCH_SUBTREE, _echo_handler, NULL },
     { "/riot/board", COAP_GET, _riot_board_handler, NULL },
     { "/riot/createkeys", COAP_GET, _create_keys_handler, NULL }, //MINE
     { "/riot/did", COAP_GET, getDidDocument, NULL }, //MINE
     { "/riot/getpublickey", COAP_GET, _get_public_key_handler, NULL }, //MINE
     { "/riot/sign", COAP_POST, ed25519_sign_handler, NULL }, //MINE
-    { "/riot/value", COAP_GET | COAP_PUT | COAP_POST, _riot_value_handler, NULL },
-    { "/riot/ver", COAP_GET, _riot_block2_handler, NULL },
-    { "/sha256", COAP_POST, _sha256_handler, NULL },
-    //{ "/riot/temperature", COAP_POST, , NULL}
 };
 
 const unsigned coap_resources_numof = ARRAY_SIZE(coap_resources);
