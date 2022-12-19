9da1de3409056cab0811d63dd33d9c90043329fe
Konstantinos Betchavas <kostasbetchavas@hotmail.com>
2022-12-19 18:55:04 +0200

    Project Restructuring

--- coap_riot/bin/native/cpu/tramp.d
+++
@@ -1,7 +0,0 @@
-/home/momus/proj_DID/coap_riot/bin/native/cpu/tramp.o: \
- /home/momus/proj_DID/RIOT/cpu/native/tramp.S /usr/include/stdc-predef.h \
- /home/momus/proj_DID/coap_riot/bin/native/riotbuild/riotbuild.h
-
-/usr/include/stdc-predef.h:
-
-/home/momus/proj_DID/coap_riot/bin/native/riotbuild/riotbuild.h:
---
+++ coap_server_riot/bin/native/base64/base64.d
@@ -0,0 +1,71 @@
+/home/momus/proj_DID/coap_server_riot/bin/native/base64/base64.o: \
+ /home/momus/proj_DID/RIOT/sys/base64/base64.c /usr/include/stdc-predef.h \
+ /home/momus/proj_DID/coap_server_riot/bin/native/riotbuild/riotbuild.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stdbool.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stdint.h /usr/include/stdint.h \
+ /usr/include/bits/libc-header-start.h /usr/include/features.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/wordsize.h \
+ /usr/include/bits/long-double.h /usr/include/gnu/stubs.h \
+ /usr/include/gnu/stubs-32.h /usr/include/bits/types.h \
+ /usr/include/bits/timesize.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/time64.h /usr/include/bits/wchar.h \
+ /usr/include/bits/stdint-intn.h /usr/include/bits/stdint-uintn.h \
+ /home/momus/proj_DID/RIOT/sys/include/base64.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stddef.h \
+ /home/momus/proj_DID/RIOT/core/lib/include/kernel_defines.h \
+ /home/momus/proj_DID/RIOT/core/lib/include/modules.h \
+ /home/momus/proj_DID/RIOT/core/lib/include/riot_version.h \
+ /home/momus/proj_DID/RIOT/core/lib/include/compiler_hints.h \
+ /home/momus/proj_DID/RIOT/core/lib/include/container.h
+
+/usr/include/stdc-predef.h:
+
+/home/momus/proj_DID/coap_server_riot/bin/native/riotbuild/riotbuild.h:
+
+/usr/lib/gcc/x86_64-linux-gnu/9/include/stdbool.h:
+
+/usr/lib/gcc/x86_64-linux-gnu/9/include/stdint.h:
+
+/usr/include/stdint.h:
+
+/usr/include/bits/libc-header-start.h:
+
+/usr/include/features.h:
+
+/usr/include/sys/cdefs.h:
+
+/usr/include/bits/wordsize.h:
+
+/usr/include/bits/long-double.h:
+
+/usr/include/gnu/stubs.h:
+
+/usr/include/gnu/stubs-32.h:
+
+/usr/include/bits/types.h:
+
+/usr/include/bits/timesize.h:
+
+/usr/include/bits/typesizes.h:
+
+/usr/include/bits/time64.h:
+
+/usr/include/bits/wchar.h:
+
+/usr/include/bits/stdint-intn.h:
+
+/usr/include/bits/stdint-uintn.h:
+
+/home/momus/proj_DID/RIOT/sys/include/base64.h:
+
+/usr/lib/gcc/x86_64-linux-gnu/9/include/stddef.h:
+
+/home/momus/proj_DID/RIOT/core/lib/include/kernel_defines.h:
+
+/home/momus/proj_DID/RIOT/core/lib/include/modules.h:
+
+/home/momus/proj_DID/RIOT/core/lib/include/riot_version.h:
+
+/home/momus/proj_DID/RIOT/core/lib/include/compiler_hints.h:
+
+/home/momus/proj_DID/RIOT/core/lib/include/container.h:
